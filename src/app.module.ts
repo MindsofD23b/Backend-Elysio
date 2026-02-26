c0c5cf2cfeab484c48ddb540f978d5a74818f113
Luis Allamand <Luis@g-12.ch>
2026-02-26 16:42:56 +0100

    feat(users): implement user module with controller, service, and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,25 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
 
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      autoLoadEntities: true,
+      synchronize: true,
+    })
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
---
+++ src/users/user.entity.ts
@@ -0,0 +1,55 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({unique: true})
+    phoneNumber: string;
+
+    @Column({default: false})
+    emailVerified: boolean;
+
+    @Column()
+    gender: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({nullable: true})
+    dateOfBirth: Date;
+
+    @Column()
+    country: string;
+
+    @Column()
+    language: string;
+
+    @Column()
+    jobTitle: string;
+
+    @Column()
+    AboutMe: string;
+
+    @Column({default: false})
+    acceptedTerms: boolean;
+
+    @Column({default: false})
+    acceptedPrivacyPolicy: boolean;
+
+    @Column({nullable: true})
+    lastLogin: Date;
+
+    @CreateDateColumn()
+    createdAt: Date;
+}
