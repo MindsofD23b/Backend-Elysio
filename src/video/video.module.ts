a653684aa57c27faca6187ef61f7f1b71d69ffc5
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2026-03-25 19:05:42 +0100

    [feat] added socket connection in the backend for videocall

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MediaService } from './media.service';
 import { VideoController } from './video.controller';
 import { VideoService } from './video.service';
+import { VideoGateway } from './video.gateway';
 
 @Module({
   controllers: [VideoController],
-  providers: [MediaService, VideoService],
+  providers: [MediaService, VideoService, VideoGateway],
 })
 export class VideoModule {}
