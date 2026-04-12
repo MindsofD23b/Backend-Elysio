145fba3ab05d09a5dd5c5d61463ef07824610bdc
Luis Allamand <Luis@g-12.ch>
2026-04-12 18:27:56 +0200

    feat: add MatchHistory entity to UsersModule imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { MatchHistory } from 'src/matchmaking/entities/match-history.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, MatchHistory])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
