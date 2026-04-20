00e0531b52f1c208466501b28f884de6c94d144d
Luis Allamand <Luis@g-12.ch>
2026-04-20 18:27:58 +0200

    fix: update R2_CLIENT import path to reference r2.module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,35 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { MatchHistory } from 'src/matchmaking/entities/match-history.entity';
 import { ProfilePicture } from './entities/profile-picture.entity';
-import { S3Client } from '@aws-sdk/client-s3';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { R2Service } from 'src/r2/r2.service';
-
-export const R2_CLIENT = 'R2_CLIENT';
+import { ConfigModule } from '@nestjs/config';
+import { R2Module } from 'src/r2/r2.module';
 
 @Module({
   imports: [
     ConfigModule,
     TypeOrmModule.forFeature([User, MatchHistory, ProfilePicture]),
+    R2Module,
   ],
   controllers: [UsersController],
-  providers: [
-    {
-      provide: R2_CLIENT,
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) =>
-        new S3Client({
-          region: 'auto',
-          endpoint: `https://${config.getOrThrow('CF_ACCOUNT_ID')}.r2.cloudflarestorage.com`,
-          credentials: {
-            accessKeyId: config.getOrThrow('R2_ACCESS_KEY_ID'),
-            secretAccessKey: config.getOrThrow('R2_SECRET_ACCESS_KEY'),
-          },
-        }),
-    },
-    R2Service,
-    UsersService,
-  ],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
