ca7c76296c2f84bae19117052ddf4e2dafbb7eea
Luis Allamand <Luis@g-12.ch>
2026-04-20 15:44:07 +0200

    fix: update R2_CLIENT import path to reference UsersModule

--- src/r2/r2.service.ts
+++ src/r2/r2.service.ts
@@ -8,7 +8,7 @@ import {
     GetObjectCommand,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { R2_CLIENT } from './r2.module';
+import { R2_CLIENT } from '../users/users.module';
 import { randomUUID } from 'crypto';
 
 @Injectable()
